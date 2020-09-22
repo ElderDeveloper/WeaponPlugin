eaec96f4ea09b9bbc0fbc5eed412dfd92ee980d9
Kemal Erdem YILMAZ <kemalerdem09@gmail.com>
2020-09-22 09:14:12 +0300

    Widget c++

--- Plugins/WeaponSystem/Source/WeaponSystem/Public/Widget/SWidgetWeapon.h
+++ Plugins/WeaponSystem/Source/WeaponSystem/Public/Widget/SWidgetWeapon.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "SWidgetWeapon.generated.h"
 
@@ -19,14 +20,36 @@ public:
     UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly)
     int32 iAmmoInBag;
 
+	UPROPERTY(BlueprintReadWrite,meta = (BindWidget))
+		class UTextBlock* CurrentAmmoText;
+
+	UPROPERTY(BlueprintReadWrite,meta = (BindWidget))
+		class UTextBlock* CurrentAmmoInBagText;
+
+	UPROPERTY(meta = (BindWidget))
+		class UImage* AimImage_01;
+
+	UPROPERTY(meta = (BindWidget))
+		class UImage* AimImage_02;
+
+	UPROPERTY(meta = (BindWidget))
+		class UImage* AimImage_03;
+
+	UPROPERTY(meta = (BindWidget))
+		class UImage* AimImage_04;
+
+	UPROPERTY(meta = (BindWidget))
+	class USizeBox* AimSizeBox;
+
     UPROPERTY(VisibleDefaultsOnly,BlueprintReadOnly)
-    class ASWeapon* aWeaponRef;
+		class ASWeapon* aWeaponRef;
+
     
     virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
     UFUNCTION(BlueprintCallable)
     void SetupWidget(class ASWeapon* weaponref);
 
-    UFUNCTION(BlueprintImplementableEvent)
+    UFUNCTION(BlueprintNativeEvent)
     void AmmoStateChanged(int32 iCurrentAmmo,int32 iCurrentAmmoInBag);
 };
